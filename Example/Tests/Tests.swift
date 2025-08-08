13e261faa37e307048e239624b8343305283ae11
Jorge Londoño <jorgemedellin1902@gmail.com>
2025-08-08 13:34:11 -0700

    Update project files and configurations

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,27 @@
 import UIKit
 import XCTest
-import SwiftQRScanner
+import SwiftQRScanner   
 
-class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class MockQRScannerDelegate: QRScannerCodeDelegate {
+    var scannedCode: String?
+
+    func qrScanner(_ controller: UIViewController, didScanResult result: String) {
+        scannedCode = result
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func qrScannerDidFail(_ controller: UIViewController, error: String) {}
+    func qrScannerDidCancel(_ controller: UIViewController) {}
+}
+
+class QRScannerTests: XCTestCase {
+    func testMockQRScan() {
+        let delegate = MockQRScannerDelegate()
+        let controller = QRScannerController()
+        controller.delegate = delegate
+
+        // Simulate a scan result (normally this would come from the camera)
+        delegate.qrScanner(controller, didScanResult: "https://example.com")
+
+        XCTAssertEqual(delegate.scannedCode, "https://example.com")
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
